00560b09d1d8fea9291cb42cd68fb2593d909fa0
Kate Culver <kateeculver@gmail.com>
2023-07-10 16:29:56 -0700

    added tailwind, colour and font set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='bg-white'>
+      <header>
+        <div className='text-purple font-bold underline font-serif'>here</div>
+        <div style={{height: '100px', width: '100px', backgroundColor: '#A4C1C6',}}></div>
+        <div style={{height: '100px', width: '100px', backgroundColor: '#3C4D58',}}></div>
+        <div style={{height: '100px', width: '100px', backgroundColor: '#6E4B5A',}}></div>
+        <div style={{height: '100px', width: '100px', backgroundColor: '#f9f5e8',}}></div>
+        <div style={{height: '100px', width: '100px', backgroundColor: '#2f2b2a',}}></div>
       </header>
+      <h1 className='text-teal font-light font-sans'>Hello my name is Kate</h1>
+      <p className='text-teal-black font-serif'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        colors: {
+            'teal': '#3C4D58',
+            'teal-light': '#A4C1C6',
+            'purple': '#6E4B5A',
+            'white': '#f9f5e8',
+            'black': '#2f2b2a',
+        },
+        fontFamily: {
+            sans: ['Montserrat', 'sans-serif'],
+            serif: ['Courier New', 'serif'],
+        },
+        extend: {},
+    },
+    corePlugins: {
+        preflight: false,
+    },
+    plugins: [],
+}
+
