f64c8d4054477f59bf30b30b8b4e12e39c8f97fc
Kate Culver <kateeculver@gmail.com>
2023-07-10 22:39:20 -0700

    style guide clean up

--- src/pages/StyleGuide.js
+++ src/pages/StyleGuide.js
@@ -1,18 +1,23 @@
 function StyleGuide() {
   return (
-    <div className='bg-white'>
-      <header>
-        <div style={{height: '100px', width: '100px', backgroundColor: '#A4C1C6',}}></div>
-        <div style={{height: '100px', width: '100px', backgroundColor: '#3C4D58',}}></div>
-        <div style={{height: '100px', width: '100px', backgroundColor: '#6E4B5A',}}></div>
-        <div style={{height: '100px', width: '100px', backgroundColor: '#f9f5e8',}}></div>
-        <div style={{height: '100px', width: '100px', backgroundColor: '#2f2b2a',}}></div>
-      </header>
-      <h1 className='text-teal font-light font-sans'>Hello my name is Kate</h1>
-      <p className='text-teal-black font-serif'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-      <button><span>Test Button</span></button>
-      <button className='button button--outline'><span>Test Button</span></button>
-      <button className='button button--outline--purple'><span>Test Button</span></button>
+    <div className="bg-white h-screen">
+        <div className="container m-auto pt-20">
+            <div>
+                <div className="bg-teal h-20 w-20 inline-block"></div>
+                <div className="bg-teal-light h-20 w-20 inline-block"></div>
+                <div className="bg-purple h-20 w-20 inline-block"></div>
+                <div className="bg-white h-20 w-20 inline-block"></div>
+                <div className="bg-black h-20 w-20 inline-block"></div>
+            </div>
+            <h1 className="text-teal font-light font-sans">Hello my name is Kate</h1>
+            <p className="text-teal-black font-serif">This is my style guide.</p>
+            <a href="/">This is a link</a>
+            <div class="grid grid-cols-4 gap-4 mt-10">
+                <div><button><span>Primary Action</span></button></div>
+                <div><button className="button button--outline"><span>Secondary Action</span></button></div>
+                <div><button className="button button--outline--purple"><span>Secondary Action</span></button></div>
+            </div>
+        </div>
     </div>
   );
 }
