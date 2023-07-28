405883bc0c4bb7e332c57b271d8e2d6330a07750
Kate Culver <kateeculver@gmail.com>
2023-07-27 17:25:52 -0700

    finished timeline styling without transitions

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,26 +1,23 @@
 function Timeline() {
     const events = [
-        {date: 202212, importance: 3, title: 'Software Engineer', company: 'Pela', event: 'Stopped working at Pela', image: 'pela.png'},
+        {date: 202212, importance: 3, title: 'Software Engineer', company: 'Pela', event: 'Left Pela', image: 'pela.png'},
         {date: 202204, hideDate: true, importance: 2, title: 'Software Engineer', company: 'Pela', event: 'Started acting as lead developer'},
-        {date: 202112, hideDate: true, importance: 2, title: 'Software Engineer', company: 'Pela', event: 'Started training and mentoring Jr. Developers'},
+        {date: 202112, hideDate: true, importance: 1, title: 'Software Engineer', company: 'Pela', event: 'Started training and mentoring Jr. Developers'},
         {date: 202107, importance: 2, title: 'Software Engineer', company: 'Pela', event: 'Promoted to Software Engineer'},
-        {date: 202012, hideDate: true, importance: 2, title: 'Software Engineer', company: 'Pela', event: 'Introduced to Node.js and AWS Lambda development'},
-        {date: 202009, hideDate: true, importance: 2, title: 'Software Engineer', company: 'Pela', event: 'Introduced to GraphQL, TypeScript, and Gatsby Cloud'},
-        {date: 202003, hideDate: true, importance: 2, title: 'Software Engineer', company: 'Pela', event: 'Introduced to Shopify and ecommerce development'},
-        {date: 202002, importance: 3, title: 'Jr. Developer', company: 'Cheeky Monkey Media', event: 'Started working at Pela', image: 'cheekymonkey.png'},
-        {date: 201911, hideDate: true, importance: 2, title: 'Intermediate Full Stack Developer', company: 'Cheeky Monkey Media', event: 'Introduced to PHP'},
-        {date: 201910, hideDate: true, importance: 3, title: 'Intermediate Full Stack Developer', company: 'Cheeky Monkey Media', event: 'Started working at Cheeky Monkey Media'},
-        {date: 201908, importance: 3, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Stopped working at ChatterBlock', image: 'chatterblock.png'},
-        {date: 201907, hideDate: true, importance: 2, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Introduced to React'},
-        {date: 201906, hideDate: true, importance: 2, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Independently designed and built front-end web application user dashboard.'},
-        {date: 201806, hideDate: true, importance: 2, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Rebuilt customer user dashboard and business listing pages'},
-        {date: 201710, hideDate: true, importance: 2, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Introduced to Django, PostgreSQL, ElasticSearch, HTML, CSS, and JavaScript'},
-        {date: 201709, hideDate: true, importance: 3, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Started working at ChatterBlock'},
-        {date: 201708, importance: 3, title: 'Bachelor of Science', company: 'University of Victoria', event: 'Graduated from University of Victoria', image: 'uvic.png'},
-        {date: 201704, hideDate: true, importance: 2, title: 'Bachelor of Science', company: 'University of Victoria', event: 'Introduced to UI/UX design and SQL'},
-        {date: 201501, hideDate: true, importance: 2, title: 'Bachelor of Science', company: 'University of Victoria', event: 'Introduced to Assembly, C, and Python'},
-        {date: 201310, hideDate: true, importance: 2, title: 'Bachelor of Science', company: 'University of Victoria', event: 'Introduced to Java and Software Engineering concepts'},
-        {date: 201309, importance: 3, title: 'Bachelor of Science', company: 'University of Victoria', event: 'Started attending University of Victoria'},
+        {date: 202012, hideDate: true, importance: 1, title: 'Software Engineer', company: 'Pela', event: 'Introduced to Node.js and AWS Lambda development'},
+        {date: 202009, hideDate: true, importance: 1, title: 'Software Engineer', company: 'Pela', event: 'Introduced to GraphQL, TypeScript, and Gatsby Cloud'},
+        {date: 202003, hideDate: true, importance: 1, title: 'Software Engineer', company: 'Pela', event: 'Introduced to Shopify and ecommerce development'},
+        {date: 202002, importance: 3, title: 'Jr. Developer', company: 'Cheeky Monkey Media', event: 'Started at Pela, left Cheeky Monkey Media', image: 'pela.png'},
+        {date: 201911, hideDate: true, importance: 1, title: 'Intermediate Full Stack Developer', company: 'Cheeky Monkey Media', event: 'Introduced to PHP'},
+        {date: 201910, importance: 3, title: 'Intermediate Full Stack Developer', company: 'Cheeky Monkey Media', event: 'Started at Cheeky Monkey Media', image: 'cheekymonkey.png'},
+        {date: 201908, importance: 3, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Left ChatterBlock', image: 'chatterblock.png'},
+        {date: 201907, hideDate: true, importance: 1, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Introduced to React'},
+        {date: 201906, hideDate: true, importance: 1, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Independently designed and built front-end web application user dashboard.'},
+        {date: 201806, hideDate: true, importance: 1, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Rebuilt customer user dashboard and business listing pages'},
+        {date: 201710, hideDate: true, importance: 1, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Introduced to Django, PostgreSQL, ElasticSearch, HTML, CSS, and JavaScript'},
+        {date: 201709, importance: 3, title: 'Full Stack Software Developer', company: 'ChatterBlock', event: 'Started at ChatterBlock', image: 'chatterblock.png'},
+        {date: 201708, importance: 3, title: 'Bachelor of Science', company: 'University of Victoria', event: 'Graduated from the University of Victoria', image: 'uvic.png'},
+        {date: 201707, hideDate: true, importance: 1, company: 'University of Victoria', event: 'Studied Math, Computer Science, and Visual Arts'},
     ]
     const months = [
         'January',
@@ -71,26 +68,30 @@ function Timeline() {
                     return (
                         <div key={index}>
                             
-                                <div className="grid grid-cols-5 max-w-[800px] m-auto h-4">
+                                <div className={`grid grid-cols-5 max-w-[560px] m-auto ${event.importance === 3 ? 'h-14' : 'h-4'}`}>
                                     <div className={`col-span-2 flex items-center relative ${event.importance !== 3 ? 'order-last' : ''}`}>
-                                        <div className={`absolute top-0 max-w-[300px] ${event.importance !== 3 ? 'left-0' : 'right-0'}`}>
-                                            <p className={`caption--small text-teal-light my-0 ${event.importance === 3 ? 'ml-auto mr-2' : 'ml-2'}`}>
-                                                {event.event}
-                                            </p>
-                                            {
-                                                !event.hideDate &&
-                                                <p className={`caption text-teal-light my-0 ${event.importance === 3 ? 'ml-auto mr-2' : 'ml-2'}`}>
-                                                    {months[event.date % 100 - 1]} {Math.floor(event.date*0.01)}
-                                                </p>
-                                            }
-                                            {
-                                                event.image ?
-                                                <div className="w-10 h-10 flex items-center justify-center rounded-full m-auto bg-white border-solid border-teal-light border-4 p-4 mt-2 mr-0">
-                                                    <img src={require(`../../static/${event.image}`)} className="max-h-full max-w-full" />
+                                        <div className={`absolute max-w-[250px] left-0 ${event.importance !== 3 ? 'top-0' : 'top-4'}`}>
+                                            <div className="flex">
+                                                {
+                                                    event.image ?
+                                                    <div className="w-8 h-8 flex items-center justify-center m-auto pr-4">
+                                                        <img src={require(`../../static/${event.image}`)} className="max-h-full max-w-full" />
+                                                    </div>
+                                                    :
+                                                    <></>
+                                                }
+                                                <div>
+                                                    <p className={`text-teal-light my-0 ${event.importance === 3 ? 'ml-auto mr-2' : 'ml-2'} ${event.importance !== 1 ? 'caption text-white' : 'caption--small'}`}>
+                                                        {event.event}
+                                                    </p>
+                                                    {
+                                                        !event.hideDate &&
+                                                        <p className={`caption font-medium text-teal-light my-0 ${event.importance === 3 ? 'ml-auto mr-2' : 'ml-2'}`}>
+                                                            {months[event.date % 100 - 1]} {Math.floor(event.date*0.01)}
+                                                        </p>
+                                                    }
                                                 </div>
-                                                :
-                                                <></>
-                                            }
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="grid grid-cols-smallcenter">
